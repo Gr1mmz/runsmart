3d7df65702038b16d2025ccd7ff55e3e2cc82607
Kraevskiy Mikhail <mixail.jazz@yandex.ru>
2021-07-06 16:18:40 +0800

    add reviews and footer + responsive for all sections

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             {
                 breakpoint: 992,
                 settings: {
-                    // dots: true,
+                    dots: true,
                     arrows: false,
                 },
             },
