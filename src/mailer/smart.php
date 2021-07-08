bf16ca846df4eca292050126625b3bc98c424e62
Kraevskiy Mikhail <mixail.jazz@yandex.ru>
2021-07-08 15:23:23 +0800

    add up button

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,13 +13,13 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'mixail.jazz.wot@gmail.com';                 // Наш логин
+$mail->Username = '';                 // Наш логин
 $mail->Password = 'vxejckyizcvypohj';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('mixail.jazz.wot@gmail.com', 'RunSmart');   // От кого письмо 
-$mail->addAddress('yisoyir333@herrain.com');     // Add a recipient
+$mail->setFrom('', 'RunSmart');   // От кого письмо 
+$mail->addAddress('');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
